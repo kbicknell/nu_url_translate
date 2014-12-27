4156eaf4e7aaa14c9793f5fdaa091f97d9af02f5
Klinton Bicknell <kbicknell@northwestern.edu>
2014-12-27 19:47:37 +0100

    moved page updates to callback to remove timeout

--- nu_url_translate.js
+++ nu_url_translate.js
@@ -1,34 +1,32 @@
 /*global chrome */
 function updateUrl(tab) {
     'use strict';
-    var newurl = '',
-        parser;
+    var parser = document.createElement("a"), // initialize parser ('a' not important)
+        doTabUpdate = function (newurl) {
+            chrome.tabs.update(tab.id, {
+                url: newurl
+            })
+        };
+
     if (tab.title === "PsycNET - Display Record") {
         chrome.tabs.executeScript(null, {
                 file: "psycnet_dr.js"
             },
             function (result) {
-                newurl = result[0];
+                doTabUpdate(result[0]);
             });
     } else if (tab.title === "PsycNET - Option to Buy") {
         chrome.tabs.executeScript(null, {
                 file: "psycnet_otb.js"
             },
             function (result) {
-                newurl = result[0];
+                doTabUpdate(result[0]);
             });
     } else {
-        parser = document.createElement("a");
         parser.href = tab.url;
         parser.hostname = parser.hostname + ".turing.library.northwestern.edu";
-        newurl = parser.href;
+        doTabUpdate(parser.href);
     }
-    
-    setTimeout(function () {
-        chrome.tabs.update(tab.id, {
-            url: newurl
-        });
-    }, 100);
 }
 
 chrome.browserAction.onClicked.addListener(function (tab) {
